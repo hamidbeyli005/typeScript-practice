3f336c96677b10949dca9e64541735c11340e836
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-18 00:36:04 +0400

    Added functions

---
+++ built/8-functions/main.js
@@ -0,0 +1,36 @@
+// function sayHello():string{
+//     return 12345        Error: Type 'number' is not assignable to type 'string'.
+// }
+function sayHello() {
+    return "Hello";
+}
+var hello = sayHello();
+console.log(hello + " world");
+console.log("-----------");
+function print_message(msg) {
+    console.log(msg);
+    // return msg               Error :Type 'string' is not assignable to type 'void'
+}
+print_message("Hello World");
+function add(a, b) {
+    return a + b;
+}
+// console.log(add("3",5))      Argument of type 'string' is not assignable to parameter of type 'number'
+console.log("-----------");
+console.log(add(3, 5)); // 8
+function multiply_add() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    return nums.reduce(function (acc, num) { return acc + num; }, 0);
+}
+console.log("-----------");
+console.log(multiply_add(1)); // 1
+console.log(multiply_add(1, 2)); // 3
+console.log(multiply_add(1, 2, 3)); // 6
+console.log(multiply_add(1, 2, 3, 4)); // 10
+console.log("-----------");
+var negative_func = function (num) { return num * -1; };
+// console.log(negative_func("12"))         Argument of type 'string' is not assignable to parameter of type 'number'
+console.log(negative_func(12)); // -12   
---
+++ dev/8-functions/main.ts
@@ -0,0 +1,48 @@
+// function sayHello():string{
+//     return 12345        Error: Type 'number' is not assignable to type 'string'.
+// }
+
+
+function sayHello(): string {
+    return "Hello"
+}
+
+const hello = sayHello()
+console.log(hello + " world")
+console.log("-----------")
+
+
+function print_message(msg: string): void {
+    console.log(msg)
+    // return msg               Error :Type 'string' is not assignable to type 'void'
+}
+
+print_message("Hello World")
+
+
+function add(a: number, b: number): number {
+    return a + b
+}
+// console.log(add("3",5))      Argument of type 'string' is not assignable to parameter of type 'number'
+console.log("-----------")
+console.log(add(3, 5))           // 8
+
+
+function multiply_add(...nums: number[]): number {
+    return nums.reduce((acc, num) => acc + num, 0)
+}
+
+console.log("-----------")
+console.log(multiply_add(1))                // 1
+console.log(multiply_add(1, 2))             // 3
+console.log(multiply_add(1, 2, 3))          // 6
+console.log(multiply_add(1, 2, 3, 4))       // 10
+console.log("-----------")
+
+
+type Negative = (num: number) => number
+
+const negative_func: Negative = (num) => num * -1
+
+// console.log(negative_func("12"))         Argument of type 'string' is not assignable to parameter of type 'number'
+console.log(negative_func(12))              // -12   
